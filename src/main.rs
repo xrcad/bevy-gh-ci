b777a327e52fa6843ff0047e59648f23600dce76
Skalamanga <anscarlett@gmail.com>
2026-02-27 21:10:33 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use bevy::asset::AssetMetaCheck;
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(AssetPlugin {
+            // Wasm builds will check for meta files (that don't exist) if this isn't set.
+            // This causes errors and even panics in web builds on itch.
+            // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
+            meta_check: AssetMetaCheck::Never,
+            ..default()
+        }))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2d);
+    commands.spawn(Sprite {
+        image: asset_server.load("ducky.png"),
+        ..Default::default()
+    });
+}
